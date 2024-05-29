a6ee46daf0ee7c9a6f13574150b6ed620b26ba5b
“himelons” <its2019@naver.com>
2024-05-29 17:21:22 +0900

    반응속도 테스트

--- 계영/4th 과제 - 반응속도 테스트/response.js
+++ 계영/4th 과제 - 반응속도 테스트/response.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
       now 배경으로 전환됨*/
         screen.classList.replace("ready", "now");
         screen.textContent = "클릭 하세요";
+        startTime = new Date();
       }, Math.floor(Math.random() * 1000) + 2000);
     } else if (screen.classList.contains("ready")) {
       clearTimeout(timeoutId);

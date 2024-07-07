3b323423a90b7c9ee90125fa151a78ac428a7b8e
smartskgus <goldnaclever@naver.com>
2024-07-07 16:38:23 +0900

    study last

--- 나현/혼공자/study.js
+++ 나현/혼공자/study.js
@@ -1,11 +1,22 @@
-try {
-  console.log("try 구문입니다");
-  const array = ["사과", "바나나", "귤"];
-  array.forEach(() => {
-    throw "예외를 강제로 발생시킵니다";
-  });
-} catch (e) {
-  console.log("catch 구문입니다");
-} finally {
-  console.log("finally 구문입니다");
+class Parent {
+  test() {
+    console.log("Parent.test() 메소드");
+  }
 }
+
+class ChildA extends Parent {
+  test() {
+    super.test();
+    console.log("ChildA.test() 메소드");
+  }
+}
+
+class ChildB extends Parent {
+  test() {
+    console.log("ChildB.test() 메소드");
+    super.test();
+  }
+}
+
+new ChildA().test();
+new ChildB().test();
